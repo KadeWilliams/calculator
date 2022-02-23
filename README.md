255479b6a777dca338474d3d726bd5b05e7fbbd3
KadeWilliams <kadewilliams0@gmail.com>
2022-02-23 17:13:29 -0600

    created a rough UI with the appropriate spacing, also created the functions to execute the mathematical operations

---
+++ README.md
@@ -0,0 +1,17 @@
+# Web Based Calculator Application
+
+This is a project for The Odin Project curriculum.
+
+FUNCTIONS
+
+1. add
+2. subtract
+3. multiply
+4. divide
+
+INTERFACE
+
+The UI of the calculator contains all of the above functions as well as numbers 0-9 in order to complete the necessary mathematical equations as well as clear and equal buttons to properly evaluate the expressions.
+
+
+
---
+++ scripts.js
@@ -0,0 +1,48 @@
+const delBtn = document.querySelector('.delBtn')
+delBtn.addEventListener('click', () => {
+    console.log('clicked!')
+})
+const clear = document.querySelector('.clear')
+
+const add = (...numbers) => {
+    console.log(typeof (numbers))
+    console.log(numbers)
+    if (numbers.length < 1) {
+        return "I'm empty"
+    }
+    let sum = 0;
+    numbers.forEach(number => {
+        sum += number;
+    });
+    return sum;
+}
+
+const subtract = (...numbers) => {
+    let difference = numbers[0];
+    for (let i = 1; i <= numbers.length - 1; i++) {
+        difference -= numbers[i];
+    }
+    return difference;
+}
+
+const multiply = (...numbers) => {
+    let product = 1;
+    numbers.forEach(number => {
+        product *= number;
+    })
+    return product;
+}
+
+const divide = (...numbers) => {
+    let quotient = numbers[0];
+    console.log(quotient)
+    for (let i = 1; i <= numbers.length - 1; i++) {
+        quotient /= numbers[i];
+    }
+    return quotient;
+}
+
+const operate = (func, ...numbers) => {
+
+    return func(...numbers)
+}
